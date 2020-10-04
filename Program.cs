3cf43d8057adf532899e1817f1e092517fb8feec
Akin Sibay <sibayakin@gmail.com>
2020-10-04 18:00:47 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ArduinoUploader.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var uploader = new ArduinoSketchUploader(
+            //new ArduinoSketchUploaderOptions()
+            //{
+            //    FileName = @"C:\Users\akins\Desktop\arduinoHex\pompa.ino",
+            //    PortName = "COM3",
+            //    ArduinoModel = ArduinoModel.Mega2560
+            //});
+
+            //uploader.UploadSketch();
+        }
+    }
+}
